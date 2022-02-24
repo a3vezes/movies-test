d12464b9d961a8fe821259fd1cc01f4615771e86
Arthur Ariza <37642667+a3vezes@users.noreply.github.com>
2022-02-23 22:49:26 -0300

    adds ILIKE to title_search Query Scope

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   has_many :ratings, dependent: :destroy
-  scope :title_search, ->(title) { where("title LIKE '%#{title}%'") }
+  scope :title_search, ->(title) { where("title ILIKE '%#{title}%'") }
 
   def self.select_with_rating
     self.select('movies.*, round(avg(ratings.grade), 2) rating').includes(:ratings  ).left_outer_joins(:ratings).group("id")
